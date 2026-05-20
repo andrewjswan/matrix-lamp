3a9b938c8fc390888f54fb83b37944f5408179c3
Andrew J.Swan <ajs.blackhole@gmail.com>
2026-05-20 23:28:26 +0300

    Effect: `Shadows` optimization (#176)

--- components/matrix_lamp/effects.h
+++ components/matrix_lamp/effects.h
@@ -4914,13 +4914,14 @@ static void shadowsRoutine() {
     loadingFlag = false;
   }
 
-  static uint16_t sPseudotime = 0;
-  static uint16_t sLastMillis = 0;
-  static uint16_t sHue16 = 0;
+  static uint16_t sPseudotime = 0U;
+  static uint16_t sLastMillis = 0U;
+  static uint16_t sHue16 = 0U;
 
   uint8_t sat8 = beatsin88(87, 220, 250);
   uint8_t brightdepth = beatsin88(341, 96, 224);
   uint16_t brightnessthetainc16 = beatsin88(203, (25 * 256), (40 * 256));
+
   uint8_t msmultiplier = beatsin88(map(modes[currentMode].Speed, 1, 255, 100, 255), 32, map(modes[currentMode].Speed, 1, 255, 60, 255));
 
   uint16_t hue16 = sHue16;
@@ -4933,26 +4934,30 @@ static void shadowsRoutine() {
 
   sLastMillis  = ms;
   sPseudotime += deltams * msmultiplier;
-  sHue16 += deltams * beatsin88(400, 5,9);
+  sHue16 += deltams * beatsin88(400, 5, 9);
   uint16_t brightnesstheta16 = sPseudotime;
+
+  const uint8_t inv_brightdepth = 255U - brightdepth;
+  const long map_low = map(effectBrightness, 32, 255, 32, 125);
+  const long map_high = map(effectBrightness, 32, 255, 125, 250);
+  const uint16_t max_led_idx = NUM_LEDS - 1U;
 
   for(uint16_t i = 0 ; i < NUM_LEDS; i++) {
     hue16 += hueinc16;
-    uint8_t hue8 = hue16 / 256;
+    uint8_t hue8 = hue16 >> 8U; // / 256
 
-    brightnesstheta16  += brightnessthetainc16;
-    uint16_t b16 = sin16(brightnesstheta16) + 32768;
+    brightnesstheta16 += brightnessthetainc16;
+    uint16_t b16 = sin16(brightnesstheta16) + 32768U;
 
-    uint16_t bri16 = (uint32_t)((uint32_t)b16 * (uint32_t)b16) / 65536;
-    uint8_t bri8 = (uint32_t)(((uint32_t)bri16) * brightdepth) / 65536;
-    bri8 += (255 - brightdepth);
+    uint16_t bri16 = ((uint32_t)b16 * b16) >> 16U;          // / 65536
+    uint8_t bri8 = ((uint32_t)bri16 * brightdepth) >> 16U;  // / 65536
 
-    CRGB newcolor = CHSV(hue8, sat8, map8(bri8, map(effectBrightness, 32, 255, 32, 125), map(effectBrightness, 32, 255, 125, 250)));
+    bri8 += inv_brightdepth;
 
-    uint16_t pixelnumber = i;
-    pixelnumber = (NUM_LEDS - 1) - pixelnumber;
+    CRGB newcolor = CHSV(hue8, sat8, map8(bri8, map_low, map_high));
 
-    nblend(leds[pixelnumber], newcolor, 64);
+    uint16_t pixelnumber = max_led_idx - i;
+    nblend(leds[pixelnumber], newcolor, 64U);
   }
 }
 #endif
