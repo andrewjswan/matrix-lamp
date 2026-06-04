1cb2a97f339fc0cb1b09f277569560d99219184e
Andrew J.Swan <ajs.blackhole@gmail.com>
2026-06-04 10:58:05 +0300

    Update effect_ticker.h

--- components/matrix_lamp/effect_ticker.h
+++ components/matrix_lamp/effect_ticker.h
@@ -431,13 +431,16 @@ static void effectsTick()
     case EFF_STARS_NIGHT:         LOW_DELAY_TICK { effTimer = millis(); StarsEffect();                         }  break;  // (135U) Звездная ночь
     #endif
     #ifdef DEF_TETRIX
-    case EFF_TETRIX:              LOW_DELAY_TICK { effTimer = millis(); tetrixRoutine();                       }  break;  // (136U) Tetrix
+    case EFF_TETRIX:              LOW_DELAY_TICK { effTimer = millis(); tetrixRoutine();                       }  break;  // (136U) Тетрикс
     #endif
     #ifdef DEF_TETRIS
-    case EFF_TETRIS:              HIGH_DELAY_TICK { effTimer = millis(); tetrisRoutine();                      }  break;  // (137U) Tetris
+    case EFF_TETRIS:              HIGH_DELAY_TICK { effTimer = millis(); tetrisRoutine();                      }  break;  // (137U) Тетрис / Песочный тетрис
+    #endif
+    #ifdef DEF_METEOR
+    case EFF_METEOR:              DYNAMIC_DELAY_TICK { effTimer = millis(); meteorRoutine();                  }  break;   // (138U) Метеор    
     #endif
     #ifdef DEF_UKRAINE
-    case EFF_UKRAINE:             DYNAMIC_DELAY_TICK { effTimer = millis(); Ukraine();                         }  break;  // (138U) Україна
+    case EFF_UKRAINE:             DYNAMIC_DELAY_TICK { effTimer = millis(); Ukraine();                         }  break;  // (139U) Україна
     #endif
   }
 }
