db161535bb07d204b9ecf09e9b534ce8a1235e21
Andrew J.Swan <ajs.blackhole@gmail.com>
2026-06-09 16:07:05 +0300

    Update effect_ticker.h

--- components/matrix_lamp/effect_ticker.h
+++ components/matrix_lamp/effect_ticker.h
@@ -439,8 +439,11 @@ static void effectsTick()
     #ifdef DEF_METEOR
     case EFF_METEOR:              LOW_DELAY_TICK { effTimer = millis(); meteorRoutine();                       }  break;  // (138U) Метеор
     #endif
+    #ifdef DEF_HOURGLASS
+    case EFF_HOURGLASS_II:        DYNAMIC_DELAY_TICK { effTimer = millis(); HourGlassRoutine();                }  break;  // (139U) Пісочний годинник II
+    #endif    
     #ifdef DEF_UKRAINE
-    case EFF_UKRAINE:             DYNAMIC_DELAY_TICK { effTimer = millis(); Ukraine();                         }  break;  // (139U) Україна
+    case EFF_UKRAINE:             DYNAMIC_DELAY_TICK { effTimer = millis(); Ukraine();                         }  break;  // (140U) Україна
     #endif
   }
 }
