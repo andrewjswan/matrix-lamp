076c86485c3c4a8a64e57569e634905ef2477f72
Andrew J.Swan <ajs.blackhole@gmail.com>
2026-08-12 13:19:52 +0300

    Update components/matrix_lamp/utility.h
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- components/matrix_lamp/utility.h
+++ components/matrix_lamp/utility.h
@@ -702,7 +702,8 @@ inline void restoreSettings() {
 #if defined(RANDOM_SETTINGS_IN_CYCLE_MODE)
 static void setModeSettings(uint8_t Scale = 0U, uint8_t Speed = 0U) {
   selectedSettings = 0U;
-  if (currentMode >= MODE_AMOUNT) return;
+  if (currentMode >= MODE_AMOUNT)
+    return;
 
   modes[currentMode].Speed = Speed ? Speed : pgm_read_byte(&defaultSettings[currentMode][1]);
   modes[currentMode].Scale = Scale ? Scale : pgm_read_byte(&defaultSettings[currentMode][2]);
